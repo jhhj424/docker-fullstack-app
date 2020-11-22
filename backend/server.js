97a4c1cba8254cd0e3a73dd9e161d473322aa846
jiho <jhhj424@naver.com>
2020-11-23 00:06:29 +0900

    MySQL, Nginx(proxy) 설정

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const app = express();
 // json 형대로 오는 요청의 본문을 해석해줄수있도록 등록
 app.use(bodyParser.json());
 
-// 테이블 생성하기
-db.pool.query(`CREATE TABLE lists (
-    id INTEGER AUTO_INCREMENT,
-    value TEXT,
-    PRIMARY KEY (id)
-)`, (err, results, fileds) => {
-    console.log('result', results)
-})
+// // 테이블 생성하기
+// db.pool.query(`CREATE TABLE lists (
+//     id INTEGER AUTO_INCREMENT,
+//     value TEXT,
+//     PRIMARY KEY (id)
+// )`, (err, results, fileds) => {
+//     console.log('result', results)
+// })
 
 // DB lists 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function(req,res) {
