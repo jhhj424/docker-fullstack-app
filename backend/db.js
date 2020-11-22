1b56325662ecde5c53c20ac7d8dc973bf87a4357
jiho <jhhj424@naver.com>
2020-11-22 15:58:54 +0900

    feat) db.js file Added

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'mysql',
+    user: 'root',
+    password: 'jhhj424',
+    database: 'myapp'
+});
+exports.pool = pool;
