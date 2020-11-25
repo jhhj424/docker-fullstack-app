29e68da0697a9efb6f6833ca3c56e5a2293f9135
jiho <jhhj424@naver.com>
2020-11-26 01:06:46 +0900

    fixed

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,6 @@ const pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+    port: process.env.MYSQL_PORT
 });
 exports.pool = pool;
